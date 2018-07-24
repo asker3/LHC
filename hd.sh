dd21981bfa68f568f8d32ee7e3ed8e876fcbf797
asker <jannocklee@foxmail.com>
2018-07-24 16:21:08 +0800

    Create hd.sh

---
+++ hd.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "CPU numbers:"
+cat /proc/cpuinfo | grep "physical id" | sort | uniq | wc -l
+echo "CPU cores:"
+cat /proc/cpuinfo | grep "cpu cores"|uniq
+echo "CPU:"
+grep "model name" /proc/cpuinfo |uniq
+echo "Memory:"
+grep MemTotal /proc/meminfo
+echo "Network numbers:"
+lspci | grep -i ethernet
+echo "MAC:"
+dmesg | grep eth
